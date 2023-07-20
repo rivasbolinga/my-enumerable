80d05e35e2c4e6d82db6d492db98c4c44ae0dc14
danielsafari143 <danielsafari143@gmail.com>
2023-07-20 21:43:14 +0200

    REFACTOR LIST.RB

--- list.rb
+++ list.rb
@@ -3,15 +3,26 @@ require_relative 'enumerable'
 # 1. Create a class MyList that has an instance variable @list.
 
 class MyList
-  include MyEnumerable
-
-  def initialize(*list)
-    @list = list
-  end
-
-  # 2. In MyList implement a method #each that yields successive members of @list and uses the MyEnumerable module.
-
-  def each(&block)
-    @list.each(&block)
+  include Enumerable
+  def initialize(*arg)
+    super(arg)
   end
 end
+
+# 2. In MyList implement a method #each that yields successive members of @list and uses the MyEnumerable module.
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? { |e| e < 5 }
+# true
+list.all? { |e| e > 5 }
+# false
+
+list.any? { |e| e == 2 }
+# true
+list.any? { |e| e == 5 }
+# false
+
+list.filter(&:even?)
+#=> [2, 4]
